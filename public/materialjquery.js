1a59e7bd1693a203171fc945760c421f42fba59e
Tara Mason <tara.mason@gmail.com>
2017-11-03 08:40:51 -0600

    header\/\navbar complete

---
+++ public/materialjquery.js
@@ -0,0 +1,31 @@
+console.log("inJS");
+
+$( document ).ready(function() {
+
+    console.log( "ready!" );
+
+    $(".dropdown-button").dropdown();
+
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+    // $(window).scroll(function() {
+    //         var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+    //         var osWork = $('#workshopsID').offset().top; // pixels to the top of div1
+    //         var htWork = $('#workshopsID').height(); // height of div1 in pixels
+    //         var osWork = $('#specialsID').offset().top; // pixels to the top of div1
+    //         var htWork = $('#specialsID').height();
+    //         // if you've scrolled further than the top of workshopsID plus it's height
+    //         // change the color. either by adding a class or setting a css property
+    //         if(scroll >= osWork - htWork ){
+    //             $('body').addClass('orangeBorder');
+    //         }
+    //         if(scroll >= osSpec - htSpec ){
+    //             $('body').addClass('tealBorder');
+    //         }
+    //     });
+
+ //collapsible on workshops page
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+});
